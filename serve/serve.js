64ee942b5cc3332cc1d5a75fbf160bbcb1a66db5
joan <13266465937@163.com>
2019-05-31 17:05:11 +0800

    init

---
+++ serve/serve.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(88);
+
+let arr = []
+io.on('connection', function(client){
+    //message 为自定义事件，供客户端调用
+	client.on('message', function(_person){
+		if(_person){
+			arr.push(_person)
+			console.log(arr)
+		}
+		// 返回信息给客户端
+		io.emit('msgObj', JSON.stringify(arr));
+	})
+})
